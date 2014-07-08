82e06aff31d8837479d06d68322ecbe2527ff46a
Logan Garrett <logan.garrett@ge.com>
2014-07-07 20:19:08 -0400

    convert to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140706054132) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "schedules", force: true do |t|
     t.text     "title"
     t.text     "body"
@@ -28,7 +31,7 @@ ActiveRecord::Schema.define(version: 20140706054132) do
     t.integer  "active",     default: 0
   end
 
-  add_index "schedules", ["user_id"], name: "index_schedules_on_user_id"
+  add_index "schedules", ["user_id"], name: "index_schedules_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -45,7 +48,7 @@ ActiveRecord::Schema.define(version: 20140706054132) do
     t.datetime "updated_at"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
